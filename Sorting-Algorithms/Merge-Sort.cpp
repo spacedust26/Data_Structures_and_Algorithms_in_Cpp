6df5b4401b1048ed96e6f2ac1899a493b1c17197
Pranamya Kulal <spacedust26@gmail.com>
2024-07-14 13:46:20 +0530

    mergesort

---
+++ Sorting-Algorithms/Merge-Sort.cpp
@@ -0,0 +1,72 @@
+//To perform merge sort
+//Is Stable, Not Inplace
+
+#include<iostream>
+using namespace std;
+
+//merge
+void merge(int *arr, int s, int e){
+  int mid = s + (e-s)/2;
+  int len1 = mid - s + 1;
+  int len2 = e - mid;
+  int *first = new int[len1];
+  int *second = new int[len2];
+
+  int arrIndex = s;
+  for(int i = 0; i < len1 ; i++){
+    first[i] = arr[arrIndex++];
+  }
+  arrIndex = mid + 1;
+  for(int i = 0; i < len2 ; i++){
+    second[i] = arr[arrIndex++];
+  }
+
+  int index1 = 0, index2 = 0;
+  arrIndex = s;
+  while(index1 < len1 && index2 < len2){
+    if(first[index1] < second[index2]){
+      arr[arrIndex++] = first[index1++];
+    }else{
+      arr[arrIndex++] = second[index2++];
+    }
+  }
+
+  while(index1 < len1){
+    arr[arrIndex++] = first[index1++];
+  }
+  while(index2 < len2){
+    arr[arrIndex++] = second[index2++];
+  }
+}
+
+//mergesort
+void mergeSort(int *arr, int s, int e){
+  if(s >= e) return;
+  int mid = s + (e - s) / 2;
+  mergeSort(arr,s, mid);
+  mergeSort(arr,mid+1,e);
+  merge(arr,s,e);
+}
+
+
+//Prints all the array elements
+void print(int arr[], int n){
+  cout << endl << "Printing the array elements: ";
+  for(int i = 0; i < n ; i++){
+    cout << arr[i] << " ";
+  }
+}
+
+//Main function
+int main(){
+  int n;
+  cout << "Enter array limit: ";
+  cin >> n;
+  int arr[20];
+  cout << "Enter the array elements: ";
+  for(int i = 0 ; i < n ; i++){
+    cin >> arr[i];
+  }
+  mergeSort(arr,0,n-1);
+  print(arr,n);
+}
